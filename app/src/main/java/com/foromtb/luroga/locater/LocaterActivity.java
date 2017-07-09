11e93f4655061022f706c8a1f9c9d7c450df0264
lromeral <luis.romeral@gmail.com>
2017-07-09 19:31:23 +0200

    Cambio nombre app

--- app/src/main/java/com/foromtb/luroga/locater/LocaterActivity.java
+++ app/src/main/java/com/foromtb/luroga/locater/LocaterActivity.java
@@ -3,12 +3,12 @@ package com.foromtb.luroga.locater;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LocaterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_locater);
     }
 
 }
