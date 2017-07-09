bc6c8f17b34f7292268bd442268f1dfc165cfb5d
lromeral <luis.romeral@gmail.com>
2017-07-09 19:00:48 +0200

    Initial commit

---
+++ app/src/main/java/com/foromtb/luroga/locater/MainActivity.java
@@ -0,0 +1,13 @@
+package com.foromtb.luroga.locater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
